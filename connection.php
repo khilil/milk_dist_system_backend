ef7f8a35548b3cd9493c0506841a68d62832bbe6
khilil <81291515+khilil@users.noreply.github.com>
2025-06-10 21:08:03 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,10 +2,11 @@
 // Database configuration
 $host = 'mysql.railway.internal'; // Docker compose માં db service name
 $username = 'root';    
-$password = 'JjNPWxcgEQWDFTKWsBLVVpRrIeeYgYrK';   
+$password = 'utGwxQOdvrghdlUigzPKWPZHXDCsSSHA';   
 $database = 'railway';   
+$port = 3306;
 
-$conn = mysqli_connect($host, $username, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database, $port);
 
 if (!$conn) {
     die("❌ Connection failed: " . mysqli_connect_error());
